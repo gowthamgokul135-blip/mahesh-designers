0e4f9b578a27363c96cb6b8c95b06149d6e4c4d2
gowthamgokul135-blip <gowthamgokul135@gmail.com>
2026-06-27 09:30:34 +0530

    Deply to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export', // Next.js-a Static HTML-ah maatha idhu dhaan mukkiyam
+//   basePath: '/unga-repository-name', // Unga GitHub repository-oda peru munaadi '/' potu tharanum
+//   images: {
+//     unoptimized: true, // GitHub Pages-la images urupadiya theriyurathuku
+//   },
+// };
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export', // Next.js-a static HTML-ah mathum
+//   basePath: '/your-repo-name', // Unga repository peru munaadi '/' potu irukanum
+//   images: {
+//     unoptimized: true, // Images theriyurathuku
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
 };
 
 export default nextConfig;
